8ee6ec4e1941cf6f947de6e2f0308717a1e986c2
WXingSong <191710097@qq.com>
2019-04-16 17:43:43 +0800

    Site updated: 2019-04-16 17:43:39

--- js/schemes/pisces.js
+++ js/schemes/pisces.js
@@ -1,1 +1,57 @@
-"use strict";$(document).ready(function(){var i=$(".sidebar-inner"),o=CONFIG.sidebar.offset||12;function e(){var t=$(".header-inner").height()+o,e=function(){var t=$("#footer"),e=$(".footer-inner"),i=t.outerHeight()-e.outerHeight();return t.outerHeight()+i}();t+($("#sidebar").height()+NexT.utils.getSidebarb2tHeight())<$("#content").height()&&(i.affix({offset:{top:t-o,bottom:e}}),i.affix("checkPosition")),$("#sidebar").css({"margin-top":t,"margin-left":"auto"})}e(),window.matchMedia("(min-width: 992px)").addListener(function(t){t.matches&&($(window).off(".affix"),i.removeData("bs.affix").removeClass("affix affix-top affix-bottom"),e())})});
+/* global NexT, CONFIG */
+
+$(document).ready(function() {
+
+  var sidebarInner = $('.sidebar-inner');
+  var sidebarOffset = CONFIG.sidebar.offset || 12;
+
+  function getHeaderOffset() {
+    return $('.header-inner').height() + sidebarOffset;
+  }
+
+  function getFooterOffset() {
+    var footer = $('#footer');
+    var footerInner = $('.footer-inner');
+    var footerMargin = footer.outerHeight() - footerInner.outerHeight();
+    var footerOffset = footer.outerHeight() + footerMargin;
+    return footerOffset;
+  }
+
+  function initAffix() {
+    var headerOffset = getHeaderOffset();
+    var footerOffset = getFooterOffset();
+    var sidebarHeight = $('#sidebar').height() + NexT.utils.getSidebarb2tHeight();
+    var contentHeight = $('#content').height();
+
+    // Not affix if sidebar taller than content (to prevent bottom jumping).
+    if (headerOffset + sidebarHeight < contentHeight) {
+      sidebarInner.affix({
+        offset: {
+          top   : headerOffset - sidebarOffset,
+          bottom: footerOffset
+        }
+      });
+      sidebarInner.affix('checkPosition');
+    }
+
+    $('#sidebar').css({ 'margin-top': headerOffset, 'margin-left': 'auto' });
+  }
+
+  function recalculateAffixPosition() {
+    $(window).off('.affix');
+    sidebarInner.removeData('bs.affix').removeClass('affix affix-top affix-bottom');
+    initAffix();
+  }
+
+  function resizeListener() {
+    var mql = window.matchMedia('(min-width: 992px)');
+    mql.addListener(function(e) {
+      if (e.matches) {
+        recalculateAffixPosition();
+      }
+    });
+  }
+
+  initAffix();
+  resizeListener();
+});
